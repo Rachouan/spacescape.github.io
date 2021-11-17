6f0358db249b3c7e47e32208db0673acf75b80ad
Rachouan Rejeb <info@rachouan.com>
2021-11-17 15:01:19 +0000

    Phaser added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='home'>
+    <div  @click="initializeGame"  class="flex" >
+      <a  href="#1"  class="btn">Initialize</a>
+    </div>
+    <ion-phaser
+      v-bind:game.prop='game'
+      v-bind:initialize.prop='initialize'
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Phaser from "phaser";
+ 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      initialize: false,
+      game: {
+        width: "100%",
+        height: "100%",
+        type: Phaser.AUTO,
+        scene: {
+          init() {
+            this.cameras.main.setBackgroundColor("#24252A");
+          },
+          create() {
+            this.helloWorld = this.add.text(
+              this.cameras.main.centerX,
+              this.cameras.main.centerY,
+              "Hello World",
+              { font: "40px Arial",  fill: "#ffffff" }
+            );
+            this.helloWorld.setOrigin(0.5);
+          },
+          update() {
+            this.helloWorld.angle += 1;
+          }
+        }
+      }
+    };
+  },
+  methods: {
+    initializeGame() {
+      this.initialize = true;
+    }
   }
-}
+}; 
 </script>
